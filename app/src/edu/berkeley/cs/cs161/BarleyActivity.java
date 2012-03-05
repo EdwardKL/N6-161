8ff42db8e0df77a64084b34056563bdf262d2c67
Jason Jiang <petrifiednightmares@gmail.com>
2012-03-04 16:09:24 -0800

    CAN LIST OUT APPS NOW

--- app/src/edu/berkeley/cs/cs161/BarleyActivity.java
+++ app/src/edu/berkeley/cs/cs161/BarleyActivity.java
@@ -1,16 +1,55 @@
 package edu.berkeley.cs.cs161;
 
-import android.app.Activity;
-import android.widget.TextView;
-import android.os.Bundle;
+import java.util.List;
 
-public class BarleyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Android. I am barley.");
-        setContentView(tv);
-    }
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class BarleyActivity extends Activity
+{
+	ArrayAdapter<String> allAppsArrayAdapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		//Find a list of all installed apps
+		final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		final List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities( mainIntent, 0);
+		
+		allAppsArrayAdapter = new ArrayAdapter<String>(this, R.layout.app_display);
+
+		
+		ListView allAppsListView = (ListView) findViewById(R.id.app_list);
+		
+		
+		allAppsListView.setAdapter(allAppsArrayAdapter);
+
+		
+		
+		if (pkgAppsList.size() > 0)
+		{
+			findViewById(R.id.app_list).setVisibility(View.VISIBLE);
+			for (ResolveInfo app: pkgAppsList)
+			{
+				allAppsArrayAdapter.add(app.activityInfo.name);
+			}
+		}
+		else
+		{
+			String noDevices = getResources().getText(R.string.no_apps).toString();
+			allAppsArrayAdapter.add(noDevices);
+		}
+		
+		
+	}
 }
