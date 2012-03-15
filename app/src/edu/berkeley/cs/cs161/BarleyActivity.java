08e4678791967ca7537c08551d83a539cd7c834f
Victor Chang <victorchang91@gmail.com>
2012-03-15 02:29:35 -0700

    Converted Barley to a gridview

--- app/src/edu/berkeley/cs/cs161/BarleyActivity.java
+++ app/src/edu/berkeley/cs/cs161/BarleyActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 
 public class BarleyActivity extends Activity
@@ -31,46 +32,21 @@ public class BarleyActivity extends Activity
 		final List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities( mainIntent, 0);
 		
 		allAppsArrayAdapter = new ArrayAdapter<String>(this, R.layout.app_display);
-
+		GridView gridview = (GridView) findViewById(R.id.gridview);
 		
-		ListView allAppsListView = (ListView) findViewById(R.id.app_list);
-
 		
-				
-		//Add click abilities
-		OnItemClickListener ocl = new OnItemClickListener(){
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+	    gridview.setAdapter(new ImageAdapter(this, pkgAppsList));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 				Intent edit = new Intent(parent.getContext(), PolicyEdit.class);
-				Object item = parent.getItemAtPosition(position);
-				edit.putExtra(PolicyEdit.APP_ID, item.toString());
+				ResolveInfo item = (ResolveInfo) parent.getItemAtPosition(position);
+				System.out.println(item.toString());
+				edit.putExtra(PolicyEdit.APP_ID, item.activityInfo.name.toString());
 				startActivityForResult(edit, ACTIVITY_EDIT);
-			}
-		};
-		allAppsListView.setOnItemClickListener(ocl);
+	        }
+	    });
 		
-		
-		allAppsListView.setAdapter(allAppsArrayAdapter);
-
-		//Add styling to the divider
-		int[] colors = {0xFF666666, 0xFFFFFFFF}; 
-		allAppsListView.setDivider(new GradientDrawable(Orientation.TOP_BOTTOM, colors));
-		allAppsListView.setDividerHeight(3);
-		
-		//Insert the names of the apps into the list
-		if (pkgAppsList.size() > 0)
-		{
-			findViewById(R.id.app_list).setVisibility(View.VISIBLE);
-
-			for (ResolveInfo app: pkgAppsList)
-			{
-				allAppsArrayAdapter.add(app.activityInfo.name);
-			}
-		}
-		else
-		{
-			String noDevices = getResources().getText(R.string.no_apps).toString();
-			allAppsArrayAdapter.add(noDevices);
-		}
 		
 		
 	}
